64d4cd51d89a43fbb7c5bbb027c5d25930ee1ff9
Le Tien Loc <locle8831@gmail.com>
2024-06-20 23:52:57 +0700

    video item new

--- src/Pages/Home/Video/HeaderVideo.tsx
+++ src/Pages/Home/Video/HeaderVideo.tsx
@@ -7,21 +7,23 @@ const cx = classNames.bind(styles);
 const HeaderVideo = () => {
   return (
     <div className={cx("wrapper-header")}>
-      <img
-        src="https://files.fullstack.edu.vn/f8-tiktok/users/6763/6664479d5e44a.jpg"
-        alt="Thumb"
-        className={cx("avt-header")}
-      />
-      <div className={cx("content-header")}>
-        <a href={`/@locle`} className={cx("content-username")}>
-          <h3>locle</h3>
-          <h4>Le Tien Loc</h4>
-        </a>
-        <p className={cx("description-header")}>Lúc cũm ít chechou lắm 🤣 </p>
-        <p className={cx("music-header")}>
-          <FontAwesomeIcon icon={faMusic} />
-          <span>Nhạc nền Triệu Tư Long</span>
-        </p>
+      <div className={cx("header-left")}>
+        <img
+          src="https://files.fullstack.edu.vn/f8-tiktok/users/6763/6664479d5e44a.jpg"
+          alt="Thumb"
+          className={cx("avt-header")}
+        />
+        <div className={cx("content-header")}>
+          <a href={`/@locle`} className={cx("content-username")}>
+            <h3>locle</h3>
+            <h4>Le Tien Loc</h4>
+          </a>
+          <p className={cx("description-header")}>Lúc cũm ít chechou lắm 🤣 </p>
+          <p className={cx("music-header")}>
+            <FontAwesomeIcon icon={faMusic} />
+            <span>Nhạc nền Triệu Tư Long</span>
+          </p>
+        </div>
       </div>
       <Button outline className={cx("follow-btn")}>
         Follow
